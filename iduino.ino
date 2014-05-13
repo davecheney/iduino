2ceba9034ca938ddd2bf63a8ed763efc4a40ed68
Dave Cheney <dave@cheney.net>
2014-05-13 22:25:56 +1000

    * Added ! command to reset via the wtd * Removed A..F synanyms for a..f

--- iduino.ino
+++ iduino.ino
@@ -1,5 +1,7 @@
 // iduinio - The interactive Arduino
 
+#include <avr/wdt.h>
+
 unsigned char R0;  // scratch register
 unsigned char* R1;  // address register
 unsigned char* R2;  // source register
@@ -39,34 +41,13 @@ void loop()  {
       R0 |= (b & 0xf);      
       break;
     case 'a':
-    case 'A':
-      R0 <<=4;
-      R0 |= 0xa;
-      break;
     case 'b':
-    case 'B':
-      R0 <<=4;
-      R0 |= 0xb;
-      break;
     case 'c':
-    case 'C':
-      R0 <<=4;
-      R0 |= 0xc;
-      break;
     case 'd':
-    case 'D':
-      R0 <<=4;
-      R0 |= 0xd;
-      break;
     case 'e':
-    case 'E':
-      R0 <<=4;
-      R0 |= 0xe;
-      break;
     case 'f':
-    case 'F':
       R0 <<=4;
-      R0 |= 0xf;
+      R0 |= ((b - 87)) & 0xf;
       break;
     case 'n':
       R1++;
@@ -96,6 +77,11 @@ void loop()  {
     case '~': // BIC the value in *R1 with R0, unsets the bits in R0.
       *R1 &= ~R0;
       break; 
+    case '!':  // reset
+      cli();                  // Clear interrupts
+      wdt_enable(WDTO_15MS);      // Set the Watchdog to 15ms
+      while(1);            // Enter an infinite loop
+      break;
     case ']':
     case '\n':
     case '\r':
@@ -106,6 +92,7 @@ void loop()  {
     Serial.write(b);
   }
 }
+
 
 
 
